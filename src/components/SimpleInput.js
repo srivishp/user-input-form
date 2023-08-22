57aa8254be9d4198ee2607bc0555960f6992ba20
Sri Vishnu <srivishp@gmail.com>
2023-08-23 00:22:42 +0530

    Added wasTouched state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,11 +2,15 @@ import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
   // true cuz it wouldn't show warning on page load
-  const [nameIsValid, setNameIsValid] = useState(true);
+  const [nameIsValid, setNameIsValid] = useState(false);
+  const [nameIsTouched, setNameIsTouched] = useState(false);
   const nameRef = useRef();
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    setNameIsTouched(true);
+
     const username = nameRef.current.value;
     if (username.trim() === "") {
       setNameIsValid(false);
@@ -18,8 +22,12 @@ const SimpleInput = (props) => {
     console.log("Form Submitted!");
   };
 
+  const nameInputIsValid = !nameIsValid && nameIsTouched;
+
   // dynamic CSS for invalid case
-  const nameInputClass = nameIsValid ? "form-control" : "form-control invalid";
+  const nameInputClass = nameInputIsValid
+    ? "form-control invalid"
+    : "form-control";
 
   // If a form is submitted and it has a button in it, a HTTP req is automatically sent to the server
   // It is a browser default behaviour, hence we use preventDefault()
@@ -28,7 +36,9 @@ const SimpleInput = (props) => {
       <div className={nameInputClass}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" ref={nameRef} />
-        {!nameIsValid && <p className="error-text">Please enter your name</p>}
+        {nameInputIsValid && (
+          <p className="error-text">Please enter your name</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
