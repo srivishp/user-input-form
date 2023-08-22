3a9ff3ef39ab049f1494c27065fe331cc5920a24
Sri Vishnu <srivishp@gmail.com>
2023-08-22 23:13:17 +0530

    Form validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,34 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  // true cuz it wouldn't show warning on page load
+  const [nameIsValid, setNameIsValid] = useState(true);
+  const nameRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const username = nameRef.current.value;
+    if (username.trim() === "") {
+      setNameIsValid(false);
+
+      return;
+    }
+    setNameIsValid(true);
+    console.log(username);
+    console.log("Form Submitted!");
+  };
+
+  // dynamic CSS for invalid case
+  const nameInputClass = nameIsValid ? "form-control" : "form-control invalid";
+
+  // If a form is submitted and it has a button in it, a HTTP req is automatically sent to the server
+  // It is a browser default behaviour, hence we use preventDefault()
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className={nameInputClass}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameRef} />
+        {!nameIsValid && <p className="error-text">Please enter your name</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
